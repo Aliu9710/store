76626762a398c60bb68bb5a56dc19bae8b16953a
Aliu9710 <89379254+Aliu9710@users.noreply.github.com>
2021-10-11 15:31:55 +0800

    HKR系统练习

---
+++ hkr注册.py
@@ -0,0 +1,25 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+driver =webdriver.Chrome()
+driver.get("http://localhost:8080/HKR/")
+driver.maximize_window()
+driver.find_element_by_xpath("/html/body/div/div/div[1]/div[2]/a[1]").click()
+driver.find_element_by_xpath("//*[@id='loginname']").send_keys("小梁")
+driver.find_element_by_xpath("//*[@id='msform']/fieldset[1]/input[2]").send_keys("小炎")
+driver.find_element_by_xpath("//*[@id='pwd']").send_keys("7416478")
+driver.find_element_by_xpath("//*[@id='msform']/fieldset[1]/input[4]").send_keys("7416478")
+driver.find_element_by_xpath("//*[@id='msform']/fieldset[1]/input[5]").click()
+driver.find_element_by_xpath("//*[@id='valid_age']").send_keys("23")
+driver.find_element_by_xpath("//*[@id='msform']/fieldset[2]/select[1]").send_keys("男")#男\女
+driver.find_element_by_xpath("//*[@id='classname']").send_keys("python自动化")
+time.sleep(3)
+driver.find_element_by_xpath("//*[@id='msform']/fieldset[2]/input[3]").click()
+driver.find_element_by_xpath("//*[@id='reg_mail']").send_keys("12256@qq.com")
+driver.find_element_by_xpath("//*[@id='reg_phone']").send_keys("18121261194")
+driver.find_element_by_xpath("//*[@id='msform']/fieldset[3]/textarea").send_keys("花总")
+driver.find_element_by_xpath("//*[@id='btn_reg']").click()
+time.sleep(3)
+driver.find_element_by_xpath("/html/body/div[2]/div[3]/a/span/span").click()
+time.sleep(5)
+driver.find_element_by_xpath("/html/body/div[2]/div[3]/a").click()
